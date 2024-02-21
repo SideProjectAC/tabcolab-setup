e2f1dba93ab2ea59f26cabed2fa239bdcca61c0b
nivek-yang <nivekyang12014@gmail.com>
2024-02-22 04:39:54 +0800

    Add databaseConfig

---
+++ app/config/databaseConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mongodb: {
+    host: "localhost",
+    port: 27017,
+    database: "myapp",
+    username: "myUser",
+    password: "myPassword",
+  },
+  jsonserver: {
+    filePath: "./db.json",
+  },
+};
